5a345e59b10d5232b89206080e9586044f809a30
Noah Pope <npope@tutanota.com>
2023-09-27 08:06:09 -0400

    creating AllCitiesListView file to begin linking & displaying all cities to their respective countries

---
+++ Realm Intro-2/Views/AllCitiesListView.swift
@@ -0,0 +1,21 @@
+//
+//  AllCitiesListView.swift
+//  Realm Intro-2
+//
+//  Created by Noah Pope on 9/27/23.
+//
+
+import SwiftUI
+import RealmSwift
+
+struct AllCitiesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AllCitiesListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AllCitiesListView()
+    }
+}
