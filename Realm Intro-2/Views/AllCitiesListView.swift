652ef9c1ad382c2e4bf454be589b691b12cc8f8e
Noah Pope <npope@tutanota.com>
2023-09-29 14:01:20 -0400

    incomplete - giving AllCitiesListView body

--- Realm Intro-2/Views/AllCitiesListView.swift
+++ Realm Intro-2/Views/AllCitiesListView.swift
@@ -11,7 +11,17 @@ import RealmSwift
 struct AllCitiesListView: View {
     @ObservedResults(City.self, sortDescriptor: SortDescriptor(keyPath: "name")) var cities
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                ForEach(cities) {city in
+                    HStack {
+                        Text(city)
+                        Spacer()
+                        Text(city.country.first?.name ?? "No Country")
+                    }
+                }
+            }
+        }
     }
 }
 
